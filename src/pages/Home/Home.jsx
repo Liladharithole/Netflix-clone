d4496975b123ef4c21d59b299ab05796f75c43af
LILADHAR <97004436+Liladharithole@users.noreply.github.com>
2025-01-29 11:13:27 +0530

    Initial Setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "./StyleHome.module.css";
+
+const Home = () => {
+  return <div className={Style.Home}>Home</div>;
+};
+
+export default Home;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from './StyleLogin.module.css';
+
+const Login = () => {
+  return (
+    <div className={Style.login}>Login</div>
+  )
+}
+
+export default Login
